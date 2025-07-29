47e05d83deb12d55ee3da1920ec904ddcebdf1c5
kirill555525 <surskillwill@gmail.com>
2025-07-29 14:31:12 +0500

    feat: task 2411

---
+++ 2411. Smallest Subarrays With Maximum Bitwise OR/2411. Smallest Subarrays With Maximum Bitwise OR.go
@@ -0,0 +1,27 @@
+package main
+
+func smallestSubarrays(nums []int) []int {
+	n := len(nums)
+	pos := make([]int, 31)
+	for i := range pos {
+		pos[i] = -1
+	}
+
+	res := make([]int, n)
+
+	for i := n - 1; i >= 0; i-- {
+		j := i
+		for bit := 0; bit < 31; bit++ {
+			if (nums[i] & (1 << bit)) == 0 {
+				if pos[bit] != -1 {
+					j = max(j, pos[bit])
+				}
+			} else {
+				pos[bit] = i
+			}
+		}
+		res[i] = j - i + 1
+	}
+
+	return res
+}

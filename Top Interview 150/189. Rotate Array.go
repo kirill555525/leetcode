3e452955c07e5eda51c8a1507075969253c970e2
kirill555525 <surskillwill@gmail.com>
2025-08-17 17:10:14 +0500

    task 189

---
+++ Top Interview 150/189. Rotate Array.go
@@ -0,0 +1,16 @@
+package main
+
+import "slices"
+
+func rotate(nums []int, k int) {
+
+	n := len(nums)
+
+	k = k % n
+
+	slices.Reverse(nums)
+
+	slices.Reverse(nums[:k])
+	slices.Reverse(nums[k:])
+
+}
